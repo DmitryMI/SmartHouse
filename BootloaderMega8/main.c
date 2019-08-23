2f3418fffa9b3103489576a1d749f5451baa6d65
DmitryMI <wet75787@tqosi.com>
2019-08-23 11:40:44 +0300

    Bootloader initial version added

---
+++ BootloaderMega8/main.c
@@ -0,0 +1,18 @@
+/*
+ * BootloaderMega8.c
+ *
+ * Created: 23.08.2019 11:40:11
+ * Author : Dmitry
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
